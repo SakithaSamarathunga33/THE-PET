9c62547d7df41e14ce7eeb8c5f63115eee60cf13
Gangani Nimeshika <ganganinimeshika2000@gmail.com>
2025-03-22 20:45:25 +0530

    Change the some files

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -9,11 +9,10 @@ exports.addPet = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
-};  
+};    
 
 // Get all pets
-exports.getPets = async (req, res) => {
-    try {
+exports.getPets = async (req, res) => {  
         const pets = await Pet.find();
         res.status(200).json(pets);
     } catch (error) {
@@ -29,7 +28,7 @@ exports.getPetById = async (req, res) => {
         res.status(200).json(pet);
     } catch (error) {
         res.status(500).json({ error: error.message });
-    }  
+    }    
 };
 
 // Update pet
@@ -40,7 +39,7 @@ exports.updatePet = async (req, res) => {
         res.status(200).json({ message: "Pet updated successfully", pet });
     } catch (error) {
         res.status(400).json({ error: error.message });
-    }
+    }  
 };  
 
 // Delete pet
@@ -51,5 +50,5 @@ exports.deletePet = async (req, res) => {
         res.status(200).json({ message: "Pet deleted successfully" });
     } catch (error) {
         res.status(500).json({ error: error.message });
-    }  
+    }    
 };
